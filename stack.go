a4d90c07779b57418d13d23efda9cca4682271a7
Frederik Suerig <54886730+frederik-suerig@users.noreply.github.com>
2022-03-02 19:41:02 +0100

    feat: implement `pop` method

--- stack.go
+++ stack.go
@@ -1,9 +1,27 @@
 package threadsafestack
 
+import "errors"
+
+const ErrEmptyStack string = "stack is empty"
+
 type StringStack struct {
 	Value []string
 }
 
 func (s *StringStack) push(value string) {
 	s.Value = append(s.Value, value)
+}
+
+func (s *StringStack) pop() (string, error) {
+	if len(s.Value) == 0 {
+		return "", errors.New(ErrEmptyStack)
+	}
+
+	lastIndex := len(s.Value) - 1
+	lastElement := s.Value[lastIndex]
+
+	s.Value = s.Value[:lastIndex]
+
+	return lastElement, nil
+
 }
